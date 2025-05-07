91a9cd942ccd5e8689924c33b2a0ad16d7248014
atmalviya <atul.m@cfalabs.com>
2025-05-07 14:56:11 +0530

    first lecture of ts

---
+++ L1/app.js
@@ -0,0 +1,8 @@
+//number,string,boolean
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 11;
+var n2 = 22;
+var ans = add(n1, n2);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,10 @@
+//number,string,boolean
+function add(num1:number,num2:number)
+{
+    return num1+num2;
+}
+
+const n1=11;
+const n2=22;
+const ans=add(n1,n2)
+console.log(ans)
